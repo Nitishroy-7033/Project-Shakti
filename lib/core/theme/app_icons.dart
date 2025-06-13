f8df0bd0dc6eb298ef1bc6c90c1a25f33d6852bb
Dukan24 <dukanmanager24@gmail.com>
2025-06-13 10:34:22 +0530

    #7  #6 theme retale

---
+++ lib/core/theme/app_icons.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  static const IconData home = Icons.home;
+  static const IconData settings = Icons.settings;
+  static const IconData profile = Icons.person;
+  static const IconData notification = Icons.notifications;
+  static const IconData logout = Icons.logout;
+  static const IconData search = Icons.search;
+  static const IconData camera = Icons.camera_alt;
+  static const IconData upload = Icons.upload;
+  static const IconData edit = Icons.edit;
+  static const IconData delete = Icons.delete;
+
+  // Custom use case icons
+  static const IconData attendance = Icons.access_time;
+  static const IconData dashboard = Icons.dashboard;
+  static const IconData calendar = Icons.calendar_today;
+}
