922c89eb6634e42e826271f23cbbcf2cc27f3a1a
codewithabhi001 <codewithabhi01@gmail.com>
2025-06-28 00:59:35 +0530

    added appcolor

--- lib/pages/AddFriendPage.dart
+++ lib/pages/AddFriendPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:contacts_service/contacts_service.dart';
+
 import 'package:permission_handler/permission_handler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'friend_list_page.dart';
@@ -16,20 +16,20 @@ class _AddFriendPageState extends State<AddFriendPage> {
   final TextEditingController _numberController = TextEditingController();
   int _selectedIndex = 0;
 
-  Future<void> _pickContact() async {
-    final permissionStatus = await Permission.contacts.request();
-    if (permissionStatus.isGranted) {
-      final contact = await ContactsService.openDeviceContactPicker();
-      if (contact != null && contact.phones!.isNotEmpty) {
-        _nameController.text = contact.displayName ?? '';
-        _numberController.text = contact.phones!.first.value ?? '';
-      }
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Contact permission denied')),
-      );
-    }
-  }
+  // Future<void> _pickContact() async {
+  //   final permissionStatus = await Permission.contacts.request();
+  //   if (permissionStatus.isGranted) {
+  //     final contact = await ContactsService.openDeviceContactPicker();
+  //     if (contact != null && contact.phones!.isNotEmpty) {
+  //       _nameController.text = contact.displayName ?? '';
+  //       _numberController.text = contact.phones!.first.value ?? '';
+  //     }
+  //   } else {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       const SnackBar(content: Text('Contact permission denied')),
+  //     );
+  //   }
+  // }
 
   Widget _buildBottomItem(IconData icon, String label, int index) {
     final isSelected = _selectedIndex == index;
@@ -140,7 +140,8 @@ class _AddFriendPageState extends State<AddFriendPage> {
                     height: 58,
                     width: 52,
                     child: ElevatedButton(
-                      onPressed: _pickContact,
+                      onPressed: () {},
+
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.grey.shade200,
                         foregroundColor: Colors.black,
@@ -185,8 +186,10 @@ class _AddFriendPageState extends State<AddFriendPage> {
                     const SizedBox(height: 12),
                     Row(
                       children: [
-                        const Icon(Icons.warning_amber_outlined,
-                            color: Colors.purple),
+                        const Icon(
+                          Icons.warning_amber_outlined,
+                          color: Colors.purple,
+                        ),
                         const SizedBox(width: 8),
                         Text(
                           "What is SOS button?",
@@ -212,9 +215,11 @@ class _AddFriendPageState extends State<AddFriendPage> {
                   onPressed: () {
                     // Add friend action
                     Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const FriendListPage()),
-    );
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const FriendListPage(),
+                      ),
+                    );
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.purple,
@@ -222,7 +227,9 @@ class _AddFriendPageState extends State<AddFriendPage> {
                       borderRadius: BorderRadius.circular(30),
                     ),
                     padding: const EdgeInsets.symmetric(
-                        horizontal: 50, vertical: 16),
+                      horizontal: 50,
+                      vertical: 16,
+                    ),
                   ),
                   child: Text(
                     'Add Friend',
